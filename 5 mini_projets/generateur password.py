1a3c3f84fed6497df45dbb4627c027e45a446823
Jimmy NIYONKURU <104285083+JimmyNIYONKURU@users.noreply.github.com>
2022-11-19 20:50:14 +0100

    Update generateur password.py

--- 5 mini_projets/generateur password.py
+++ 5 mini_projets/generateur password.py
@@ -3,7 +3,6 @@ from datetime import datetime
 now=datetime.now()
 print("Continue till you get your password")
 longpass=int(input("How long do you want your password to be? "))
-#v contient tous les caractères dans lesquels  le mot de passe sera tiré
 v="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%&()*+,-./:;<=>?@[]^_`{|}"
 password="".join(random.sample(v,longpass))
 print(f"On {now} you get this password: {password}. Please keep it or change it if you want the one you will be able to memorize easily")
